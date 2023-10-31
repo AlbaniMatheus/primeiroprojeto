838e6a7db2c57a2eaf88cccd244c961a56a34ee4
AlbaniMatheus <134014657+AlbaniMatheus@users.noreply.github.com>
2023-10-31 19:17:12 -0300

    Add files via upload

--- Teste.java
+++ Teste.java
@@ -10,11 +10,13 @@ public class Teste {
         System.out.println("Tipo de conta:       Conta Corrente");
         System.out.println("Saldo inicial:       R$ "+ saldoBancario);
         System.out.println("***************************************");
+        System.out.println(" ");
 
         int botao = 0;
 
         while (botao != 4) {
-            System.out.println("Operações");
+            System.out.println("Operações:");
+            System.out.println(" ");
             System.out.println("1- Consultar saldo");
             System.out.println("2- Receber valor");
             System.out.println("3- Transferir valor");
@@ -22,9 +24,33 @@ public class Teste {
 
             botao = scanner.nextInt();
 
+            if (botao == 1){
+                System.out.println("O saldo atual é R$ " + saldoBancario);
+                System.out.println(" ");
+            }
+            else if (botao == 2) {
+                double recebeDinheiro = 0 ;
+                System.out.println("Informe o valor a receber:");
+                recebeDinheiro = scanner.nextDouble();
+                saldoBancario = saldoBancario + recebeDinheiro;
+                System.out.println("Saldo atualizado R$ "+ saldoBancario);
+            }
+            else if (botao ==3) {
+                double perdeDinheiro = 0;
+                System.out.println("Informe o valor que deseja transferir:");
+                perdeDinheiro = scanner.nextDouble();
+                if (perdeDinheiro > saldoBancario){
+                    System.out.println("Não há saldo suficiente para fazer essa transferência");
+                }
+                else {
+                    saldoBancario = saldoBancario - perdeDinheiro;
+                    System.out.println("Saldo atualizado R$ "+ saldoBancario);
+                }
 
-
-
+            }
+            else if (botao != 4){
+                System.out.println("Opção inválida");
+            }
 
 
         }
